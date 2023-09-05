40d3f6be2b6b28e383b44ba250d2db535b800250
Kulamani prahdhan <km99pradhan@gmail.com>
2023-09-05 12:17:40 +0530

    styling done

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Book = require('./book')
 
 const authorSchema = new mongoose.Schema({
   name: {
@@ -6,5 +7,33 @@ const authorSchema = new mongoose.Schema({
     required: true
   }
 })
+
+authorSchema.pre('deleteOne', function(next) {
+  Book.find({ author: this.id }, (err, books) => {
+    if (err) {
+      next(err)
+    } else if (books.length > 0) {
+      next(new Error('This author has books still'))
+    } else {
+      next()
+    }
+  })
+})
+// authorSchema.pre("deleteOne", function (next) {
+//   console.log("Inside pre");
+//   Book.find({ author: this.id }, (err, books) => {
+//     console.log("inside book.find");
+//     if (err) {
+//       console.log("inside if(err)");
+//       next(err);
+//     } else if (books.length > 0) {
+//       console.log("inside book.length");
+//       next(new Error("This author has books still"));
+//     } else {
+//       console.log("inside last else");
+//       next();
+//     }
+//   });
+// });
 
 module.exports = mongoose.model('Author', authorSchema)
--- routes/authors.js
+++ routes/authors.js
@@ -1,7 +1,7 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
-
+const Book = require('../models/book')
 
 // All Authors Route
 router.get('/', async (req, res) => {
@@ -19,26 +19,101 @@ router.get('/', async (req, res) => {
     res.redirect('/')
   }
 })
-// New Author Routes
 
-router.get('/new',(req,res)=>{
-    res.render('authors/new',{author: new Author()})
+// New Author Route
+router.get('/new', (req, res) => {
+  res.render('authors/new', { author: new Author() })
 })
-//post route
-router.post('/',async(req,res)=>{
-    const author = new Author({
-        name: req.body.name
+
+// Create Author Route
+router.post('/', async (req, res) => {
+  const author = new Author({
+    name: req.body.name
+  })
+  try {
+    const newAuthor = await author.save()
+    res.redirect(`authors/${newAuthor.id}`)
+  } catch {
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating Author'
     })
-    try {
-        const newAuthor = await author.save()
-        // res.redirect(`authors/${newAuthor.id}`)
-        res.redirect(`authors`)
-      } catch {
-        res.render('authors/new', {
-          author: author,
-          errorMessage: 'Error creating Author'
-        })
-      }
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    const books = await Book.find({ author: author.id }).limit(6).exec()
+    res.render('authors/show', {
+      author: author,
+      booksByAuthor: books
     })
+  } catch {
+    res.redirect('/')
+  }
+})
+
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    res.render('authors/edit', { author: author })
+  } catch {
+    res.redirect('/authors')
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    author.name = req.body.name
+    await author.save()
+    res.redirect(`/authors/${author.id}`)
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.render('authors/edit', {
+        author: author,
+        errorMessage: 'Error updating Author'
+      })
+    }
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  let author
+  try {
+    author = await Author.findById(req.params.id)
+    await author.deleteOne()
+    res.redirect('/authors')
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.redirect(`/authors/${author.id}`)
+    }
+  }
+})
+
+// router.delete("/:id", async (req, res) => {
+//   let author;
+//   try {
+//     author = await Author.findById(req.params.id);
+//     console.log(author);
+//     await author.deleteOne();
+//     console.log("author deleted");
+//     res.redirect("/authors");
+//   } catch {
+//     if (author == null) {
+//       console.log("author not found");
+//       res.redirect("/");
+//     } else {
+//       console.log("can't delete author");
+//       res.redirect(`/authors/${author.id}`);
+//     }
+//   }
+// });
 
 module.exports = router
